4864754f6cfbc6a8e167e88cbd3b975f6e8153df
Omar-Pecos <48331054+Omar-Pecos@users.noreply.github.com>
2019-03-08 02:32:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mislibros_omar/Elemento.java
@@ -0,0 +1,13 @@
+package com.example.mislibros_omar;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Elemento extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_element);
+    }
+}
